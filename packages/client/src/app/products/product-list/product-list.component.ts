aea1318a62935d5cf1d4adc2644383b01b5b2fd3
kayumuzzaman <i.kayumuzzaman@gmail.com>
2022-08-04 12:24:07 +0600

    skeleton of product component

---
+++ packages/client/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core'
+
+/* eslint-disable */
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
